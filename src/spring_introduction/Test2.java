537274e0302dde8ad3b6e7b6c8085c606b8a2e66
SadykovIF <ifs@front.ru>
2022-10-08 17:39:39 +0500

    Learn spring

---
+++ src/spring_introduction/Test2.java
@@ -0,0 +1,15 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myCat", Pet.class);
+        pet.say();
+        Pet pet1 = context.getBean("myDog", Pet.class);
+        pet1.say();
+        context.close();
+    }
+}
